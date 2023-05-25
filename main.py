ffb7fce213244222b5a3e84e97abf4314204ee4a
Michael Primavera <pastasauce59@gmail.com>
2023-05-25 10:13:40 -0400

    Created a successful SMTP connection that uses the datemodule to find if the day is monday and if it is, randomly chooses a motivational quote from the quotes.txt file and sends it to desired recipient.

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
-# import smtplib
-# from s_data import *
-
-# email = my_email
-# password = my_password
-
-# with smtplib.SMTP("smtp.gmail.com") as connection: #Standard Mail Transfer Protocol
-#     #Transport Layer Security
-#     connection.starttls()
-#     connection.login(user=email, password=password)
-#     connection.sendmail(
-#         from_addr=email,
-#         to_addrs=to_sender,
-#         msg="Subject:App Test\n\nHELLO WORLD FROM AN EMAIL PYTHON APP"
-#         )
-#     # connection.close() #Unnecessary if used with the "with - as..."
-
-
+from s_data import *
+import smtplib
 import datetime as dt
+import random
+
+email = my_email
+password = my_password
 
 now = dt.datetime.now()
-year = now.year
-if year == 2023:
-    print("TWENTY 20 🌲")
-month = now.month
-weekday = now.weekday()
-# print(type(now)) # prints a datetime class
-print(weekday) #computers count from 0 so '1' would be te second day of the week
+day_of_week = now.weekday()
 
-date_of_birth = dt.datetime(year=1111, month=1, day=1, hour=4)
-print(date_of_birth)
+if day_of_week == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes) 
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=email, password=password)
+        connection.sendmail(
+            from_addr=email,
+            to_addrs=to_sender,
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+            )
